557dcf7fce188b3f770fb9d45960c84e4d5a9559
xiaoyang <1611037570@qq.com>
2025-12-16 10:54:08 +0800

    refactor: 重构用户模块常量结构并添加图标字段

--- src/constants/modules/user.ts
+++ src/constants/modules/user.ts
@@ -1,22 +1,24 @@
 export const DEV_ACCOUNT = [
-  [
-    {
-      name: 'gitHub',
-      url: 'https://github.com/1611037570',
-    },
-    {
-      name: '稀土掘金',
-      url: 'https://juejin.cn/user/2342386827791687/posts',
-    },
-  ],
+  {
+    name: 'gitHub',
+    url: 'https://github.com/1611037570',
+    icon: 'fa6-brands:github',
+  },
+  {
+    name: '稀土掘金',
+    url: 'https://juejin.cn/user/2342386827791687/posts',
+    icon: 'simple-icons:juejin',
+  },
 ]
 export const SHOOT_ACCOUNT = [
   {
     name: '抖音',
     url: 'https://www.douyin.com/user/MS4wLjABAAAA2ZiMi4shkb1NyOFV-2DhkMCEUirye08iiO5tO1gq6a5RHwszFi_OdZ_ssU0B3N-u',
+    icon: 'logos:tiktok-icon',
   },
   {
     name: '小红书',
     url: 'https://www.xiaohongshu.com/user/profile/6772591e00000000180175f9',
+    icon: 'simple-icons:xiaohongshu',
   },
 ]
