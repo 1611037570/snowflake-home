bff31c70021739df864312bb6db9131af49f7bfc
xiaoyang <1611037570@qq.com>
2025-12-19 08:30:29 +0800

    refactor(utils/url): 提取获取基础 URL 逻辑到独立函数

--- src/utils/modules/url.ts
+++ src/utils/modules/url.ts
@@ -1,11 +1,13 @@
+export function getBaseUrl() {
+  const routerMode = import.meta.env.VITE_ROUTER_MODE
+  const flag = routerMode === 'hash' ? '#' : ''
+  return window.location.origin + flag
+}
 export function routerNavigation(url: string) {
   if (!url) return
   // 初始化 url 路径
   url = url.startsWith('/') ? url : '/' + url
-  const originUrl = window.location.origin
-  const routerMode = import.meta.env.VITE_ROUTER_MODE
-  const flag = routerMode === 'hash' ? '#' : ''
-  const lastUrl = originUrl + flag + url
+  const lastUrl = getBaseUrl() + url
   window.open(lastUrl, '_blank')
 }
 export function urlNavigation(url: string) {
