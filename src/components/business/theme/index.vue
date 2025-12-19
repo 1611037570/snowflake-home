8db77670cb3d485d157522f9770a79691816cc75
xiaoyang <1611037570@qq.com>
2025-12-19 08:48:02 +0800

    feat(主题切换): 将主题切换从工具提示改为下拉菜单

--- src/components/business/theme/index.vue
+++ src/components/business/theme/index.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="text-sf-text" @click="toggleTheme">
-    <SfTooltip :content="themeMode === 'dark' ? '切换为浅色主题' : '切换为暗黑主题'">
+    <!-- <SfTooltip :content="themeMode === 'dark' ? '切换为浅色主题' : '切换为暗黑主题'">
       <SfIcon
         :icon="themeMode === 'dark' ? 'twemoji:sun' : 'ri:moon-clear-fill'"
         size="8"
         style="color: #f3d776"
       />
-    </SfTooltip>
+    </SfTooltip> -->
+    <ElDropdown trigger="hover">
+      <SfIcon
+        :icon="themeMode === 'dark' ? 'twemoji:sun' : 'ri:moon-clear-fill'"
+        size="8"
+        style="color: #f3d776"
+      />
+      <template #dropdown>
+        <el-dropdown-menu>
+          <SfList
+            class="w-[120px]"
+            :list="list"
+            activeKey="value"
+            @onClick="handleClick"
+            :activeValue="themeMode"
+          >
+            <template #default="{ item }">
+              {{ item.name }}
+            </template>
+          </SfList>
+        </el-dropdown-menu>
+      </template>
+    </ElDropdown>
   </div>
 </template>
 
@@ -20,11 +42,15 @@ const toggleTheme = () => {
   setTheme(themeMode.value)
 }
 // // 主题选项
-// const list = ref([
-//   { name: '跟随系统', value: 'default' },
-//   { name: '暗黑模式', value: 'dark' },
-//   { name: '简约白色', value: 'light' },
-// ])
+const list = ref([
+  { name: '跟随系统', value: 'default' },
+  { name: '暗黑模式', value: 'dark' },
+  { name: '简约白色', value: 'light' },
+])
+
+function handleClick(item) {
+  setTheme(item.value)
+}
 </script>
 
 <style scoped></style>
