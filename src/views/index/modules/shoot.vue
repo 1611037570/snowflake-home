205a01f79091ec87bdddbf606884acef38d1a519
xiaoyang <1611037570@qq.com>
2025-12-19 09:10:08 +0800

    feat(摄影模块): 添加作品展示功能并更新城市列表

--- src/views/index/modules/shoot.vue
+++ src/views/index/modules/shoot.vue
@@ -1,17 +1,29 @@
 <template>
   <!-- 摄影社交平台 -->
-  <div class="w-dwh flex-c relative z-10 min-h-dvh flex-col bg-sf-primary py-24" id="shoot">
+  <div class="w-dwh flex-c relative z-10 min-h-dvh flex-col bg-sf-primary py-20" id="shoot">
     <h4 class="group mb-3 flex items-center justify-center text-base font-medium text-sf-text">
       <SfIcon
         icon="lucide:camera"
-        class="mr-2 h-4 w-4 text-sf-theme transition-transform duration-300 group-hover:scale-110"
+        size="8"
+        class="mr-2 text-sf-theme transition-transform duration-300 group-hover:scale-110"
       />
-      摄影作品分享
+      部分作品展示
     </h4>
+    <div class="flex flex-wrap items-center justify-center gap-2 md:gap-3">
+      <template v-for="item in [...imgList].reverse()" :key="item.id">
+        <ElImage
+          :src="item.img"
+          alt=""
+          class="h-[240px] w-[160px] rounded-xl md:h-[300px] md:w-[200px]"
+        />
+      </template>
+    </div>
 
-    <p class="mb-2 text-center text-sm text-sf-text-2">2023年6月3号 - 至今</p>
-    <p class="mb-3 text-center text-sm text-sf-text-2">旅拍城市</p>
-
+    <p class="my-3 text-center text-sf-text-2">
+      已旅拍城市:<span class="text-sf-theme">
+        {{ city.length }}
+      </span>
+    </p>
     <!-- 城市列表：增强动画 -->
     <div class="mb-4 flex flex-wrap justify-center gap-2">
       <span
@@ -23,27 +35,19 @@
       </span>
     </div>
 
-    <!-- 摄影平台链接 -->
-    <div>全部摄影作品</div>
+    <div>全部作品</div>
     <div class="grid grid-cols-2 gap-3">
-      <a
-        v-for="(item, index) in shoot.list"
+      <div
+        v-for="(item, index) in SHOOT_ACCOUNT"
         :key="index"
         @click.prevent="urlNavigation(item.url)"
         class="group relative flex overflow-hidden rounded-xl border border-sf-theme-hover/20 bg-white p-4 shadow-sm transition-all duration-300 hover:-translate-y-1 hover:border-sf-theme/30 hover:bg-sf-theme/5 hover:shadow-md"
       >
         <SfIcon
-          :icon="index === 0 ? 'simple-icons:xiaohongshu' : 'logos:tiktok-icon'"
-          class="mr-3 h-5 w-5 text-sf-theme transition-all duration-300 group-hover:scale-110"
+          :icon="item.icon"
+          class="mr-3 h-5 w-5 transition-all duration-300 group-hover:scale-110"
         />
-        <span
-          class="text-sm font-medium text-sf-text transition-colors duration-300 group-hover:text-sf-theme"
-          >{{ item.name }}</span
-        >
-        <div
-          class="absolute inset-0 bg-gradient-to-r from-sf-theme/10 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-        ></div>
-      </a>
+      </div>
     </div>
   </div>
 </template>
@@ -51,16 +55,63 @@
 <script setup>
 import { SHOOT_ACCOUNT } from '@/constants'
 import { urlNavigation } from '@/utils'
-const shoot = {
-  name: '摄影',
-  list: SHOOT_ACCOUNT,
+const components = import.meta.glob('../../../assets/images/shoot/*', { eager: true })
+
+function getImg(id) {
+  for (const key of Object.keys(components)) {
+    const matchNum = key.match(/(\d+)/)
+    if (matchNum && Number(matchNum[1]) === id) {
+      return components[key].default
+    }
+  }
 }
+const imgList = ref([
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 7,
+  },
+  {
+    id: 10,
+  },
+  {
+    id: 11,
+  },
+  {
+    id: 15,
+  },
+])
+const init = ref(false)
+
+function initImgList() {
+  for (const item of imgList.value) {
+    item.img = getImg(item.id)
+  }
+  init.value = true
+}
+initImgList()
 const city = [
   {
     name: '杭州',
   },
   {
     name: '嘉兴',
+  },
+  {
+    name: '桐乡',
+  },
+  {
+    name: '海盐',
+  },
+  {
+    name: '嘉善',
   },
   {
     name: '绍兴',
@@ -79,6 +130,12 @@ const city = [
   },
   {
     name: '平潭',
+  },
+  {
+    name: '深圳',
+  },
+  {
+    name: '长沙',
   },
   {
     name: '柳州',
