f7b9461436767d13069a372d7ea1bdc355e417ed
xiaoyang <1611037570@qq.com>
2025-12-19 09:10:32 +0800

    feat(首页): 添加内容动画和滚动功能

--- src/views/index/modules/index.vue
+++ src/views/index/modules/index.vue
@@ -1,19 +1,74 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const showContent = ref(false)
+
+onMounted(() => {
+  showContent.value = true
+})
+function togglePlay() {
+  // 获取目标元素
+  console.log('about', document.getElementById('about'))
+  const targetElement = document.getElementById('about')
+  if (targetElement) {
+    // 平滑滚动到目标元素
+    targetElement.scrollIntoView({
+      behavior: 'smooth', // 平滑滚动
+      block: 'start', // 对齐到区块顶部
+      duration: 10,
+    })
+  }
+}
+</script>
 
 <template>
   <div class="w-dwh flex-c min-h-dvh flex-col"></div>
-  <div class="w-dwh flex-c data-damu-container fixed inset-0 z-1 min-h-dvh flex-col">
+  <div
+    class="w-dwh flex-c data-barrage-container fixed inset-0 z-10 min-h-dvh flex-col overflow-hidden"
+  >
     <SfBarrage />
-    <div class="sf-text-glow text-sf-text" :class="$s(18)">代码与光影的创造者</div>
-    <div class="h-1 w-24 rounded-[4px] bg-sf-theme shadow-xl" :class="$s(8, 'my')"></div>
-    <div class="text-sf-text-2" :class="$s(7)">用代码创造数字世界，用镜头记录美好瞬间。</div>
+    <div
+      class="sf-text-glow font-semibold text-sf-text transition-all duration-300 ease-out"
+      :class="$s(18)"
+      :style="{
+        opacity: showContent ? 1 : 0,
+        transform: showContent ? 'translateY(0)' : 'translateY(20px)',
+      }"
+    >
+      代码与光影的创造者
+    </div>
+    <div
+      class="h-1 w-24 rounded-full bg-sf-theme-hover shadow-xl"
+      :class="$s(8, 'my')"
+      :style="{ opacity: showContent ? 1 : 0, width: showContent ? '6rem' : '0' }"
+    ></div>
+    <div
+      class="text-sf-text-2 transition-all duration-300 ease-out"
+      :class="$s(7)"
+      :style="{
+        opacity: showContent ? 1 : 0,
+        transform: showContent ? 'translateY(0)' : 'translateY(20px)',
+      }"
+    >
+      用代码创造数字世界，用镜头记录美好瞬间。
+    </div>
+    <div class="flex-c absolute bottom-2 left-0 w-full text-sf-base">
+      <SfTooltip>
+        <template #content>
+          <div class="flex-c items-center justify-center">继续浏览</div>
+        </template>
+        <SfIcon icon="mingcute:down-line" size="16" class="animate-bounce" @click="togglePlay" />
+      </SfTooltip>
+    </div>
   </div>
 </template>
-<script setup></script>
+
 <style lang="scss" scoped>
 .sf-text-glow {
   text-shadow:
-    0 0 18px var(--sf-theme),
-    0 0 38px var(--sf-theme);
+    0 0 16px var(--sf-theme-hover),
+    0 0 34px var(--sf-theme),
+    0 0 46px var(--sf-theme);
+  transition: text-shadow 0.3s ease;
 }
 </style>
