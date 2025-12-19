8e27aad4222b9e0e0ef1a0a715b76db579201a2f
xiaoyang <1611037570@qq.com>
2025-12-19 08:30:00 +0800

    feat(用户模块): 更新开发者账号和个人账号配置

--- src/constants/modules/user.ts
+++ src/constants/modules/user.ts
@@ -1,13 +1,25 @@
+import { getBaseUrl } from '@/utils/modules/url'
 export const DEV_ACCOUNT = [
-  {
-    name: 'gitHub',
-    url: 'https://github.com/1611037570',
-    icon: 'fa6-brands:github',
-  },
   {
     name: '稀土掘金',
     url: 'https://juejin.cn/user/2342386827791687/posts',
     icon: 'simple-icons:juejin',
+    qrcode: true,
+    type: 'dev',
+  },
+  {
+    name: 'gitHub',
+    url: 'https://github.com/1611037570',
+    icon: 'simple-icons:github',
+    qrcode: false,
+    type: 'dev',
+  },
+  {
+    name: '我的简历',
+    url: getBaseUrl() + '/resume?id=1611037570',
+    icon: 'basil:file-user-solid',
+    qrcode: false,
+    type: 'shoot',
   },
 ]
 export const SHOOT_ACCOUNT = [
@@ -15,10 +27,14 @@ export const SHOOT_ACCOUNT = [
     name: '抖音',
     url: 'https://www.douyin.com/user/MS4wLjABAAAA2ZiMi4shkb1NyOFV-2DhkMCEUirye08iiO5tO1gq6a5RHwszFi_OdZ_ssU0B3N-u',
     icon: 'logos:tiktok-icon',
+    qrcode: true,
+    type: 'shoot',
   },
   {
     name: '小红书',
     url: 'https://www.xiaohongshu.com/user/profile/6772591e00000000180175f9',
     icon: 'simple-icons:xiaohongshu',
+    qrcode: true,
+    type: 'shoot',
   },
 ]
