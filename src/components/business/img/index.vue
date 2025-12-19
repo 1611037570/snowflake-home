4e7f1792b49979a2ee2a992ba719e3a9d6a3fc13
xiaoyang <1611037570@qq.com>
2025-12-19 09:39:53 +0800

    feat(图片组件): 新增图片组件

---
+++ src/components/business/img/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Component :is="h(ElImage, { ...$attrs, ref: changeRef }, $slots)" />
+</template>
+
+<script setup lang="ts">
+import { ElImage } from 'element-plus'
+import type { ComponentInstance } from 'vue'
+import { getCurrentInstance, h } from 'vue'
+const vm: any = getCurrentInstance()
+
+function changeRef(exports: any) {
+  vm.exposed = exports
+}
+defineExpose({} as ComponentInstance<typeof ElImage>)
+</script>
+
+<style scoped></style>
