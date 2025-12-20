24d8e4df502db79f7d7ae00afb1f106f132b00c5
xiaoyang <1611037570@qq.com>
2025-12-20 08:58:12 +0800

    feat(音乐播放器): 增强音乐播放功能并添加歌词支持

--- src/views/index/components/music.vue
+++ src/views/index/components/music.vue
@@ -4,28 +4,51 @@ const musicList = [
   {
     name: '主角',
     singer: '马里奥',
-    songName: 'protagonist.mp3',
-    url: import('@/assets/audios/protagonist.mp3'),
+    songName: '主角-马里奥.aac',
+    url: import('@/assets/audios/主角-马里奥.aac'),
+    lrc: import('@/assets/audios/主角-马里奥.lrc?raw'),
   },
   {
     name: '送你一朵小红花(Live)',
     singer: '易烊千玺',
-    songName: 'ALittleRedFlower.m4a',
-    url: import('@/assets/audios/ALittleRedFlower.m4a'),
+    songName: '送你一朵小红花(Live)-易烊千玺.m4a',
+    url: import('@/assets/audios/送你一朵小红花(Live)-易烊千玺.m4a'),
+    lrc: import('@/assets/audios/送你一朵小红花(Live)-易烊千玺.lrc?raw'),
+  },
+  {
+    name: '夜空最亮的星',
+    singer: '邓紫棋',
+    songName: '夜空最亮的星-邓紫棋.m4a',
+    url: import('@/assets/audios/夜空最亮的星-邓紫棋.m4a'),
+    lrc: import('@/assets/audios/夜空最亮的星-邓紫棋.lrc?raw'),
+  },
+  {
+    name: '骄傲(Live)',
+    singer: '王源&陈芃瑄',
+    songName: '骄傲(Live)-王源&陈芃瑄.m4a',
+    url: import('@/assets/audios/骄傲(Live)-王源&陈芃瑄.m4a'),
+    lrc: import('@/assets/audios/骄傲(Live)-王源&陈芃瑄.lrc?raw'),
   },
 ]
 const currentIndex = ref(0)
-const loadAudio = (index) => {
+const loadAudio = async (index) => {
   currentIndex.value = index
   const audio = musicList[index]
   audio.url.then((module) => {
     console.log('音频资源加载成功:', module.default)
     audioUrl.value = module.default
   })
+  audio.lrc.then((module) => {
+    console.log('歌词资源加载成功:', module.default)
+    audioLrc.value = module.default
+  })
+  // const response = await fetch(audio.lrc)
+  // const lrc = await response.text()
+  // console.log('歌词资源加载成功:', lrc)
 }
 // 更换当前歌曲
 const changeCurrentMusic = () => {
-  const index = currentIndex.value ? 0 : 1
+  const index = currentIndex.value === musicList.length - 1 ? 0 : currentIndex.value + 1
   loadAudio(index)
 }
 const currentMusic = computed(() => musicList[currentIndex.value])
@@ -34,6 +57,7 @@ loadAudio(currentIndex.value)
 const isPlaying = ref(false)
 const audioPlayer = ref(null)
 const audioUrl = ref('')
+const audioLrc = ref('')
 // 加载音频文件
 const togglePlay = () => {
   if (isPlaying.value) {
@@ -99,7 +123,6 @@ const list = computed(() => {
   ></audio>
   <ElDropdown trigger="hover">
     <SfIcon
-      @click="togglePlay"
       size="6"
       boxSize="8"
       :icon="isPlaying ? 'lucide:pause' : 'lucide:play'"
