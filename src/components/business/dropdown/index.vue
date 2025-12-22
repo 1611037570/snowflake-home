a509094689934ca41eff61fc6931fe5a94ed2f1d
xiaoyang <1611037570@qq.com>
2025-12-22 08:37:34 +0800

    feat(组件): 新增业务下拉框组件基础实现

---
+++ src/components/business/dropdown/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <Component
+    :is="h(ElDropdown, { ...$attrs, ref: changeRef }, $slots)"
+    :effect="theme"
+    :trigger="trigger"
+  >
+    <template #default>
+      <slot name="default"></slot>
+    </template>
+    <template #dropdown>
+      <slot name="dropdown"></slot>
+    </template>
+  </Component>
+</template>
+
+<script setup lang="ts">
+import { ElDropdown } from 'element-plus'
+import type { ComponentInstance } from 'vue'
+import { getCurrentInstance, h } from 'vue'
+
+import { useThemeStore } from '@/stores'
+import { storeToRefs } from 'pinia'
+const trigger = ref('hover')
+const themeStore = useThemeStore()
+const { theme } = storeToRefs(themeStore)
+
+const vm: any = getCurrentInstance()
+
+function changeRef(exports: any) {
+  vm.exposed = exports
+}
+defineExpose({} as ComponentInstance<typeof ElDropdown>)
+</script>
+
+<style scoped></style>
