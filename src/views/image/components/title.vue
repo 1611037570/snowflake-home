b935273583ecab0b1176150a1b3079dce2fb3c81
xiaoyang <1611037570@qq.com>
2025-12-23 09:02:44 +0800

    feat(图片视图): 添加标题组件基础结构

---
+++ src/views/image/components/title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex w-full items-center justify-between">
+    <div class="flex flex-1">
+      <span class="text-sm font-medium"></span>
+      <SfIcon
+        icon="material-symbols:restart-alt"
+        size="5"
+        class="hover:text-sf-theme"
+        @click.stop="reset"
+      />
+    </div>
+
+    <!-- <div class="flex items-center gap-1 text-xs">
+        <span class="text-xs">{{ $t('image.keepAspect') }}</span>
+        <ElSwitch v-model="keepAspectRatio" />
+      </div> -->
+  </div>
+</template>
+
+<script setup>
+defineProps({ name: { type: String, default: '' } })
+function reset() {}
+</script>
