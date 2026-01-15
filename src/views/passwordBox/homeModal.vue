4b284ea4069731bf64f874a6a15424fdc601e774
xiaoyang <1611037570@qq.com>
2026-01-15 09:36:53 +0800

    feat(密码管理): 添加密码管理模块基础结构

---
+++ src/views/passwordBox/homeModal.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { usePasswordStore } from '@/stores/modules/password'
+const passwordStore = usePasswordStore()
+const { homeModalVisible, showPassword } = storeToRefs(passwordStore)
+const item = {
+  name: '微信',
+  password: '123456',
+}
+</script>
+
+<template>
+  <SfModal v-if="homeModalVisible" v-model="homeModalVisible">
+    <div>明文显示：<ElSwitch v-model="showPassword" /></div>
+    <ElScrollbar>
+      <div>
+        <div>{{ item.name }}</div>
+        <div>{{ showPassword ? item.password : '******' }}</div>
+      </div>
+    </ElScrollbar>
+  </SfModal>
+</template>
+
+<style lang="scss" scoped></style>
