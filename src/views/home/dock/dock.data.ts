4cdabcc683e49081bf51546091fc98004ea819bd
xiaoyang <1611037570@qq.com>
2026-01-16 08:39:29 +0800

    refactor(dock): 使用异步组件优化应用启动性能

--- src/views/home/dock/dock.data.ts
+++ src/views/home/dock/dock.data.ts
@@ -1,26 +1,61 @@
 import { ALL_PAGE_LIST } from '@/constants'
-import About from './components/about.vue'
-import AppCenter from './components/appCenter.vue'
-import Email from './components/email.vue'
-import Image from './components/image.vue'
-import Launchpad from './components/launchpad.vue'
-import Note from './components/note.vue'
-import PasswordBox from './components/passwordBox.vue'
-import SendResume from './components/sendResume.vue'
-import Setting from './components/setting.vue'
-import WriteResume from './components/writeResume.vue'
+import { defineAsyncComponent } from 'vue'
 
 const list = [
-  { name: '启动台', component: Launchpad, type: 'system' },
-  { name: '图片', component: Image, type: 'page', page: 'image' },
-  { name: '写简历', component: WriteResume, type: 'page', page: 'resume' },
-  { name: '投简历', component: SendResume, type: 'system' },
-  { name: '关于', component: About, type: 'system' },
-  { name: '设置', component: Setting, type: 'system' },
-  { name: '便签', component: Note, type: 'page', page: 'note' },
-  { name: '邮箱', component: Email, type: 'system' },
-  { name: '添加', component: AppCenter, type: 'system' },
-  { name: '密码箱', component: PasswordBox, type: 'page', page: 'passwordBox' },
+  {
+    name: '启动台',
+    component: defineAsyncComponent(() => import('./components/launchpad.vue')),
+    type: 'system',
+  },
+  {
+    name: '图片',
+    component: defineAsyncComponent(() => import('./components/image.vue')),
+    type: 'page',
+    page: 'image',
+  },
+  {
+    name: '写简历',
+    component: defineAsyncComponent(() => import('./components/writeResume.vue')),
+    type: 'page',
+    page: 'resume',
+  },
+  {
+    name: '投简历',
+    component: defineAsyncComponent(() => import('./components/sendResume.vue')),
+    type: 'system',
+  },
+  {
+    name: '关于',
+    component: defineAsyncComponent(() => import('./components/about.vue')),
+    type: 'system',
+  },
+  {
+    name: '设置',
+    component: defineAsyncComponent(() => import('./components/setting.vue')),
+    type: 'system',
+  },
+  {
+    name: '便签',
+    component: defineAsyncComponent(() => import('./components/note.vue')),
+    type: 'page',
+    page: 'note',
+  },
+  {
+    name: '邮箱',
+    component: defineAsyncComponent(() => import('./components/email.vue')),
+    type: 'system',
+  },
+  {
+    name: '添加',
+    component: defineAsyncComponent(() => import('./components/appCenter.vue')),
+    type: 'system',
+  },
+  {
+    name: '密码箱',
+    component: defineAsyncComponent(() => import('./components/passwordBox.vue')),
+    type: 'page',
+    page: 'passwordBox',
+  },
 ]
 function hasRoute(page: string) {
   return ALL_PAGE_LIST.includes(page)
