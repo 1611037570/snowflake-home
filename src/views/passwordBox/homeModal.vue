affd5ffd1f3b513222bc122dbfeb251085d22767
xiaoyang <1611037570@qq.com>
2026-01-16 08:32:37 +0800

    refactor(密码箱): 重构密码箱组件结构，提取公共逻辑

--- src/views/passwordBox/homeModal.vue
+++ src/views/passwordBox/homeModal.vue
@@ -1,22 +1,13 @@
 <script setup>
 import { usePasswordStore } from '@/stores/modules/password'
+import PasswordBox from './passwordBox.vue'
 const passwordStore = usePasswordStore()
-const { homeModalVisible, showPassword } = storeToRefs(passwordStore)
-const item = {
-  name: '微信',
-  password: '123456',
-}
+const { homeModalVisible } = storeToRefs(passwordStore)
 </script>
 
 <template>
   <SfModal v-if="homeModalVisible" v-model="homeModalVisible">
-    <div>明文显示：<ElSwitch v-model="showPassword" /></div>
-    <ElScrollbar>
-      <div>
-        <div>{{ item.name }}</div>
-        <div>{{ showPassword ? item.password : '******' }}</div>
-      </div>
-    </ElScrollbar>
+    <PasswordBox />
   </SfModal>
 </template>
 
--- src/views/passwordBox/index.vue
+++ src/views/passwordBox/index.vue
@@ -1,7 +1,67 @@
-<script setup></script>
+<script setup>
+import { useCopy } from '@/hooks/modules/useCopy'
+import { usePasswordStore } from '@/stores'
+import { storeToRefs } from 'pinia'
+import { computed, ref } from 'vue'
+
+const passwordStore = usePasswordStore()
+const { showPassword } = storeToRefs(passwordStore)
+
+// 模拟数据
+const list = ref([
+  {
+    name: '微信',
+    account: '13000000000',
+    password: '123456',
+    url: '',
+  },
+  {
+    name: 'QQ',
+    account: '13000000000',
+    password: '123456',
+    url: '',
+  },
+])
+
+// 类型列表 - 只返回第一个唯一类型
+const typeList = computed(() => {
+  // 过滤出唯一的类型名称
+  const uniqueTypes = new Set(list.value.map((item) => item.name))
+  // 如果有类型，只返回第一个
+  return uniqueTypes.size > 0 ? [Array.from(uniqueTypes)[0]] : []
+})
+</script>
 
 <template>
-  <div></div>
+  <SfViewContainer>
+    <div class="flex-c h-full w-full">
+      <div class="border-sf-b h-120 w-120 border">
+        <h1>密码箱</h1>
+        <div>明文显示：<ElSwitch v-model="showPassword" /></div>
+        <SfScrollbarTab :list="typeList" class="bg-sf-bg-hover" />
+        <ElScrollbar>
+          <div v-for="(item, index) in list" :key="index" class="mb-2 rounded-xl bg-sf-bg p-1">
+            <div class="flex items-center">
+              平台：{{ item.name }}
+              <SfIcon name="link" class="ml-2" v-if="item.url" />
+            </div>
+            <div class="cursor-pointer" @click="useCopy(item.account)">
+              账号：
+              <span class="hover:text-sf-theme-hover">
+                {{ item.account }}
+              </span>
+            </div>
+            <div class="cursor-pointer" @click="useCopy(item.password)">
+              密码：
+              <span class="hover:text-sf-theme-hover">
+                {{ showPassword ? item.password : '******' }}
+              </span>
+            </div>
+          </div>
+        </ElScrollbar>
+      </div>
+    </div>
+  </SfViewContainer>
 </template>
 
 <style lang="scss" scoped></style>
