038ec0b41870494cde837d19d11ad3281d72c37a
xiaoyang <1611037570@qq.com>
2026-01-16 08:31:16 +0800

    refactor(常量): 重构常量配置并添加用户路由

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,16 +1,19 @@
 /**
  * 系统主题色
+ * 用于系统中主要的交互元素
  */
 export const THEME_COLOR = '#50a2ff'
 /**
  * 系统主题色-悬停
+ * 用于系统中交互元素的悬停状态
  */
 export const THEME_COLOR_HOVER = 'oklch(88.2% 0.059 254.128)'
+
 /**
  * 基础页面
  * 用于展示项目的图标、主题样式资源
  */
-export const BASE_PAG_LIST = ['messageBoard', 'icon', 'color']
+export const BASE_PAG_LIST = ['messageBoard', 'icon', 'color', 'components']
 /**
  * 项目页面
  * 用于加载所需要的项目
@@ -29,10 +32,14 @@ export const PROJECT_PAGE_LIST = [
 ]
 /**
  * 所有页面
- * 包含基础页面和项目页面（路由通过 ALL_PAGE_LIST 配置）
+ * 用于展示所有项目页面和基础页面
  */
 export const ALL_PAGE_LIST = [...BASE_PAG_LIST, ...PROJECT_PAGE_LIST]
 /**
- * 默认页面
+ * 默认路由
  */
-export const DEFAULT_PAGE = '/home'
+export const DEFAULT_ROUTE = '/home'
+/**
+ * 链接打开方式
+ */
+export const URL_OPEN_TYPE = '_blank'
