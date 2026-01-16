48bd54b36a3411a0dbe46a030659632b63299899
xiaoyang <1611037570@qq.com>
2026-01-16 09:09:23 +0800

    feat(数据统计): 添加数据统计页面展示视图、组件和图标数量

---
+++ src/views/data/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ICON_LIST } from '@/constants'
+const iconLength = Object.keys(ICON_LIST).length
+// 所有视图组件
+const view = import.meta.glob('@/views/*/index.vue', { eager: false })
+const viewLength = Object.keys(view).length
+// 所有基础组件
+const baseComponent = import.meta.glob('@/components/base/*/index.vue', { eager: false })
+const baseComponentLength = Object.keys(baseComponent).length
+// 所有业务组件
+const businessComponent = import.meta.glob('@/components/business/*/index.vue', { eager: false })
+const businessComponentLength = Object.keys(businessComponent).length
+// 所有组件
+const componentLength = baseComponentLength + businessComponentLength
+</script>
+
+<template>
+  <SfViewContainer>
+    已加载 {{ viewLength }} 个视图, {{ componentLength }} 个组件, {{ iconLength }} 个图标
+  </SfViewContainer>
+</template>
+
+<style lang="scss" scoped></style>
