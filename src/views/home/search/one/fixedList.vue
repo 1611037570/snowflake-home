07265016125e514c898fb7f61c278b32462b0094
xiaoyang <1611037570@qq.com>
2026-01-19 09:20:20 +0800

    feat(固定列表): 添加取消置顶功能并优化组件结构

--- src/views/home/search/one/fixedList.vue
+++ src/views/home/search/one/fixedList.vue
@@ -6,20 +6,46 @@ const { fixedList } = storeToRefs(shortcutStore)
 function handleClick(item) {
   urlNavigation(item.url)
 }
+
+// 取消置顶
+const currentItem = ref(null)
+const list = computed(() => {
+  return [
+    {
+      name: '取消置顶',
+      fn: () => {
+        currentItem.value.top = false
+      },
+    },
+  ]
+})
+function updateCurrentItem(item) {
+  currentItem.value = item
+}
 </script>
 
 <template>
-  <SearchTitle title="已固定" icon="tabler:history" iconClass="text-sf-theme" />
-  <div class="grid grid-cols-8 gap-4">
-    <div
+  <SearchTitle
+    title="已固定"
+    icon="tabler:history"
+    iconClass="text-sf-theme"
+    v-if="fixedList.length"
+  />
+  <div class="grid grid-cols-8 gap-4" v-if="fixedList.length">
+    <SfMenu
       @click="handleClick(item)"
+      @onMounted="updateCurrentItem(item)"
+      :list="list"
       v-for="item in fixedList"
       :key="item.id"
-      class="flex-c cursor-pointer flex-col overflow-hidden rounded-xl bg-white/80 p-2 text-sm text-sf-text backdrop-blur-sm hover:bg-sf-theme-hover"
     >
-      <SfMixImg :type="item.imgType" :value="item.imgValue" :size="24" />
-      <!-- <div class="mt-2 w-full truncate px-1 text-center text-xs">{{ item.name }}</div> -->
-    </div>
+      <div
+        class="flex-c cursor-pointer flex-col overflow-hidden rounded-xl bg-white/80 p-2 text-sm text-sf-text backdrop-blur-sm hover:bg-sf-theme-hover"
+      >
+        <SfMixImg :type="item.imgType" :value="item.imgValue" :size="24" />
+        <!-- <div class="mt-2 w-full truncate px-1 text-center text-xs">{{ item.name }}</div> -->
+      </div>
+    </SfMenu>
   </div>
 </template>
 <style scoped></style>
