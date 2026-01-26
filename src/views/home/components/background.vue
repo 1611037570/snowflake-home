d7a5df338703a8b5e7aa67c2efb384aaec0c1446
xiaoyang <1611037570@qq.com>
2026-01-26 11:20:57 +0800

    feat(background): 优化背景图片加载逻辑并更新样式
    - 使用动态导入和图片预加载优化背景图片加载
    - 将背景颜色从 bg-blue-50 更新为 sf-primary
    - 将背景图片设置为计算属性，支持动态切换

--- src/views/home/components/background.vue
+++ src/views/home/components/background.vue
@@ -2,7 +2,7 @@
   <div
     @click="handleClick"
     @contextmenu="handleContextMenu"
-    class="fixed top-0 right-0 bottom-0 left-0 h-full w-full bg-blue-50 bg-cover bg-center transition-all duration-200"
+    class="fixed top-0 right-0 bottom-0 left-0 h-full w-full bg-sf-primary bg-cover bg-center transition-all duration-200"
     :class="[searchFocus ? 'z-50' : 'z-1']"
     :style="{
       filter: searchFocus || tabIndex == 1 ? 'blur(10px)' : 'blur(0px)',
@@ -12,7 +12,7 @@
           : searchFocus || tabIndex == 1
             ? 'scale(1.1)'
             : 'scale(1)',
-      backgroundImage: `url(${bg1})`,
+      backgroundImage: backgroundImage,
     }"
   ></div>
 </template>
@@ -20,14 +20,37 @@
 <script setup>
 // background-image: url('https://cn.bing.com/th?id=OHR.SunbeamsForest_ZH-CN5358008117_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp');
 // https://convertio.co/zh/download/f6fd6966623acbcef28ae90b08ca0be62189ed/
-import bg1 from '@/assets/images/background1.webp'
 import { useHomeStore, useSearchStore } from '@/stores'
 import { storeToRefs } from 'pinia'
+import { computed, onMounted, ref } from 'vue'
+
 const searchStore = useSearchStore()
 const homeStore = useHomeStore()
 const { tabIndex } = storeToRefs(homeStore)
-
 const { searchFocus } = storeToRefs(searchStore)
+
+// 背景图片URL，默认为空
+const backgroundUrl = ref('')
+
+// 计算背景图片样式
+const backgroundImage = computed(() => {
+  return backgroundUrl.value ? `url(${backgroundUrl.value})` : 'none'
+})
+
+onMounted(() => {
+  setTimeout(async () => {
+    const bgModule = await import('@/assets/images/background1.webp')
+    // 创建图片对象进行预加载
+    const img = new Image()
+    img.onload = () => {
+      // 图片完全加载后再设置为背景图
+      backgroundUrl.value = bgModule.default
+    }
+    // img.onerror = (error) => {
+    // }
+    img.src = bgModule.default
+  }, 0)
+})
 
 const handleClick = () => {
   if (tabIndex.value == 0) {
@@ -39,6 +62,7 @@ const handleClick = () => {
   }
   tabIndex.value = 0
 }
+
 const handleContextMenu = () => {
   if (tabIndex.value == 1) {
     return
