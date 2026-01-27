91b3bd177edaf1bd5c6416a784bd3c9947cd87d6
xiaoyang <1611037570@qq.com>
2026-01-27 09:17:53 +0800

    refactor(国际化): 重构页脚组件并使用国际化文本
    - 移除硬编码的文本，改用国际化字段
    - 简化页脚布局和样式
    - 删除未使用的属性和方法

--- src/components/business/footer/index.vue
+++ src/components/business/footer/index.vue
@@ -1,39 +1,26 @@
 <template>
   <footer
     class="flex-c relative flex-col overflow-hidden bg-sf-primary px-3 py-1 text-sf-text backdrop-blur-sm"
-    :class="gapClass"
   >
     <div class="flex items-center gap-3">
       <div class="flex items-center">
-        Copyright © 2019-{{ currentYear }} .
-        <span @click="userNavigation" class="ml-2 cursor-pointer font-medium hover:text-sf-theme">
-          小羊</span
-        >
-        <span class="px-1">.</span>
-        All Rights Reserved.
+        {{ $t('copyright') }} © 2019-{{ currentYear }}
+        <span @click="userNavigation" class="mx-1 cursor-pointer font-medium hover:text-sf-theme">
+          {{ $t('userName') }}
+        </span>
+        {{ $t('allRightsReserved') }}
       </div>
       <div @click="aboutVisible = true" class="cursor-pointer font-medium hover:text-sf-theme">
         关于网站
       </div>
       <div class="cursor-pointer font-medium hover:text-sf-theme">版权声明</div>
     </div>
-    <div class="flex min-w-[200px] items-center" :class="gapClass">
-      <div>
-        服务支持：
-        <span @click="defaultNavigation" class="cursor-pointer font-medium hover:text-sf-theme"
-          >雪花起始页</span
-        >
-      </div>
+    <div class="flex items-center gap-3">
       <div @click="donationVisible = true" class="cursor-pointer font-medium hover:text-sf-theme">
-        赞助支持
+        支持项目
       </div>
     </div>
 
-    <div class="flex flex-col" :class="gapClass">
-      <!-- <div class="group flex cursor-pointer items-center hover:text-sf-theme" @click="goRepo">
-        项目仓库
-      </div> -->
-    </div>
     <Donation v-model="donationVisible" v-if="donationVisible" />
     <Banner v-if="banner" />
     <About v-if="aboutVisible" v-model="aboutVisible" />
@@ -41,24 +28,17 @@
 </template>
 
 <script setup lang="ts">
-import { defaultNavigation, userNavigation } from '@/utils'
+import { userNavigation } from '@/utils'
 import dayjs from 'dayjs'
 import { computed, ref } from 'vue'
 import About from './about.vue'
 import Banner from './banner.vue'
 import Donation from './donation.vue'
-const props = defineProps({
-  gap: {
-    type: Number,
-    default: 1,
-  },
+defineProps({
   banner: {
     type: Boolean,
     default: true,
   },
-})
-const gapClass = computed(() => {
-  return `gap-${props.gap}`
 })
 
 const currentYear = computed(() => {
@@ -67,10 +47,6 @@ const currentYear = computed(() => {
 
 const donationVisible = ref(false)
 const aboutVisible = ref(false)
-
-// function goRepo() {
-//   urlNavigation('https://github.com/1611037570/snowflake-index')
-// }
 </script>
 
 <style lang="scss" scoped></style>
