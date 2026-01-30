581e0e3e4b99eeb981abeae581b5245eae69abf1
xiaoyang <1611037570@qq.com>
2026-01-30 15:09:06 +0800

    feat(表单): 添加快捷方式导入配置
    添加配置对象以支持快捷方式导入功能，包含按钮点击事件和显示文本

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,8 +1,36 @@
-<script setup></script>
+<script setup>
+const config = [
+  {
+    type: 'object',
+    component: 'setItem',
+    data: {
+      key: 'title',
+      name: 'modelValue',
+      onClick: () => {
+        console.log('点击了导入按钮')
+      },
+    },
+    props: {
+      title: '快捷方式导入',
+      info: '仅导入所有快捷方式',
+      modelValue: '导入',
+      type: 'button',
+    },
+  },
+  // {
+  //   type: 'object',
+  //   component: 'input',
+  //   props: {
+  //     name: '对象名称',
+  //     age: '年龄',
+  //   },
+  // },
+]
+</script>
 
 <template>
   <SfViewContainer>
-    <SfDynamicForm></SfDynamicForm>
+    <SfDynamicForm :config="config" class="w-100"></SfDynamicForm>
   </SfViewContainer>
 </template>
 
