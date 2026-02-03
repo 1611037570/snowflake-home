a66c410f1d3502b13c65991e17c23d069cc14606
xiaoyang <1611037570@qq.com>
2026-02-03 09:43:15 +0800

    feat(组件): 新增滚动条组件基础实现
    添加滚动条组件的基本结构和功能，包括模板定义、脚本逻辑和样式占位

---
+++ src/components/el/scrollbar/scrollbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <Component :is="h(ElScrollbar, { ...$attrs, ref: changeRef }, $slots)" class="flex-1" />
+</template>
+
+<script setup lang="ts">
+import { ElScrollbar } from 'element-plus'
+import type { ComponentInstance } from 'vue'
+import { getCurrentInstance, h } from 'vue'
+
+defineOptions({ name: 'SfScrollbar' })
+
+const vm: any = getCurrentInstance()
+
+function changeRef(exports: any) {
+  vm.exposed = exports
+}
+defineExpose({} as ComponentInstance<typeof ElScrollbar>)
+</script>
+
+<style scoped></style>
