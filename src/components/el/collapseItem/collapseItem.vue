ebe5fc445801e85643962beb42fab97ac7068b4c
xiaoyang <1611037570@qq.com>
2026-02-09 08:43:52 +0800

    feat(components): 为 Collapse 和 CollapseItem 组件添加全宽样式

--- src/components/el/collapseItem/collapseItem.vue
+++ src/components/el/collapseItem/collapseItem.vue
@@ -1,20 +1,19 @@
 <template>
-  <Component :is="h(ElCollapse, { ...$attrs, ref: changeRef }, $slots)" class="w-full" />
+  <Component :is="h(ElCollapseItem, { ...$attrs, ref: changeRef }, $slots)" class="w-full" />
 </template>
 
 <script setup lang="ts">
-import { ElCollapse } from 'element-plus'
+import { ElCollapseItem } from 'element-plus'
 import type { ComponentInstance } from 'vue'
 import { getCurrentInstance, h } from 'vue'
 
-defineOptions({ name: 'SfCollapse' })
-
+defineOptions({ name: 'SfCollapseItem' })
 const vm: any = getCurrentInstance()
 
 function changeRef(exports: any) {
   vm.exposed = exports
 }
-defineExpose({} as ComponentInstance<typeof ElCollapse>)
+defineExpose({} as ComponentInstance<typeof ElCollapseItem>)
 </script>
 
 <style scoped></style>
