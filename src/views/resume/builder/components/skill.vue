83a58e2075fbf26a514379c9b57dc219a1a5b48a
xiaoyang <1611037570@qq.com>
2026-03-01 16:54:28 +0800

    feat(简历构建器): 新增专业技能组件
    添加专业技能编辑组件，使用折叠面板和富文本编辑器，支持用户输入和编辑专业技能内容。

---
+++ src/views/resume/builder/components/skill.vue
@@ -0,0 +1,16 @@
+<script setup>
+const skill = defineModel('skill', {
+  type: String,
+  default: '',
+})
+</script>
+
+<template>
+  <SfCollapse>
+    <SfCollapseItem title="专业技能">
+      <SfWangEdit v-model="skill"></SfWangEdit>
+    </SfCollapseItem>
+  </SfCollapse>
+</template>
+
+<style lang="scss" scoped></style>
