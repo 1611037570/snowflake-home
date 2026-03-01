ec2b0720e5d1068afc7eb280d72229ab4f41c784
xiaoyang <1611037570@qq.com>
2026-03-01 14:00:43 +0800

    feat(组件): 新增 SfSlider 组件封装
    封装 Element Plus 的 ElSlider 组件，提供统一的组件引用暴露机制

---
+++ src/components/el/slider/slider.vue
@@ -0,0 +1,20 @@
+<template>
+  <Component :is="h(ElSlider, { ...$attrs, ref: changeRef }, $slots)" class="flex-1" />
+</template>
+
+<script setup lang="ts">
+import { ElSlider } from 'element-plus'
+import type { ComponentInstance } from 'vue'
+import { getCurrentInstance, h } from 'vue'
+
+defineOptions({ name: 'SfSlider' })
+
+const vm: any = getCurrentInstance()
+
+function changeRef(exports: any) {
+  vm.exposed = exports
+}
+defineExpose({} as ComponentInstance<typeof ElSlider>)
+</script>
+
+<style scoped></style>
