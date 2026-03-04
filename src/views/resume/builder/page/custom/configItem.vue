01fe15c13e46a2751b80e8aa520976a636f4416b
xiaoyang <1611037570@qq.com>
2026-03-04 08:51:18 +0800

    refactor(简历构建器): 将配置项组件从数值滑块改为列表索引滑块

--- src/views/resume/builder/page/custom/configItem.vue
+++ src/views/resume/builder/page/custom/configItem.vue
@@ -1,12 +1,14 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
   label: {
     type: String,
     required: true,
   },
-  max: {
-    type: Number,
-    default: 2,
+  list: {
+    type: Array,
+    required: true,
   },
   leftLabel: {
     type: String,
@@ -19,12 +21,21 @@ defineProps({
 })
 
 const modelValue = defineModel()
+
+const currentIndex = computed({
+  get() {
+    return props.list.findIndex((item) => item.value === modelValue.value)
+  },
+  set(index) {
+    modelValue.value = props.list[index].value
+  },
+})
 </script>
 
 <template>
   <div class="flex w-full flex-col gap-2">
     <div class="mb-2 text-base font-bold text-sf-text">{{ label }}</div>
-    <SfSlider :show-tooltip="false" show-stops :max="max" v-model="modelValue" />
+    <SfSlider :show-tooltip="false" show-stops :max="list.length - 1" v-model="currentIndex" />
     <div class="mt-2 flex items-center justify-between text-xs opacity-60">
       <div>{{ leftLabel }}</div>
       <div>{{ rightLabel }}</div>
