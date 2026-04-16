70e95dad57b7fd4b7a3ae1117714d671feb2887a
y <1611037570@qq.com>
2026-04-16 21:30:25 +0800

    feat(resume): 添加用户链接字段并更新示例数据
    - 在 UserData 接口和默认数据中添加 link 字段，用于存储个人链接（如 GitHub、掘金）
    - 更新示例简历数据，补充个人链接和教育经历详情

--- src/stores/modules/resume/dataConfig.ts
+++ src/stores/modules/resume/dataConfig.ts
@@ -6,6 +6,10 @@ export interface UserData {
     email: string
     workTime: string
     sex: string
+    link: {
+      name: string
+      url: string
+    }[]
   }
   skill?: string
   education?: {
@@ -39,6 +43,7 @@ export const DEFAULT_USER_DATA: UserData = {
     email: '',
     workTime: '',
     sex: '',
+    link: [],
   },
   skill: '',
   education: [
@@ -78,6 +83,16 @@ export const userData: UserData = {
     email: '161****570@qq.com',
     workTime: '2022.08.01',
     sex: '男',
+    link: [
+      {
+        name: 'github',
+        url: 'https://github.com/1611037570',
+      },
+      {
+        name: '稀土掘金',
+        url: 'https://juejin.cn/user/2342386827791687/posts',
+      },
+    ],
   },
   skill:
     '<p><strong>1、</strong>精通前端开发核心技术栈，熟练掌握前端技术栈，包括<strong>HTML5</strong>、<strong>CSS3</strong>精准实现产品原型图；结合<strong>ChatGPT</strong>、<strong>Copilot</strong>等AI工具实现高效开发。</p><p><strong>2、</strong>熟练使用<strong>JavaScript、TypeScript（</strong>ES6）进行现代化前端开发，掌握<strong>Axios、Loadsh</strong>等经典库的使用。</p><p><strong>3、</strong>熟练使用 <strong>Vue2和Vue3 和 VueRouter</strong>、<strong>Pinia、VueUse</strong>等vue生态框架，具有丰富的移动端和PC前台和后台项目实战经验，能够根据业务需求选择最优技术方案。</p><p><strong>4、</strong>熟练使用 <strong>UniApp </strong>跨平台开发框架，有多个同时兼容H5、小程序、App多端实战项目经验。</p><p><strong>5、</strong>熟练使用<strong>ElementPlus</strong>、<strong>AntDesignVue</strong>、<strong>Uview</strong>、<strong>Echarts</strong>等主流UI组件库的使用，能根据项目需求对组件进行深度定制和功能扩展，提高开发效率。</p><p><strong>6</strong>、完整参与过多个大型前端项目的<strong>0-1</strong>的全生命周期管理，具备独立负责项目的能力。</p><p><strong>7、</strong>熟练使用<strong>Git</strong>、<strong>SVN</strong>进行版本控制和团队协作。</p><p><strong>8、</strong>掌握<strong>Postman</strong>、<strong>Apifox </strong>等接口调试工具进行前后端联调；了解<strong>Node.js</strong>和对数据库的<strong>CURD</strong>相关知识，能与后端流畅沟通。</p><p><strong>9、</strong>具备严谨的编程思维和良好的代码规范意识，编写的代码接口清晰，可维护性强，有一定抗压能力。</p>',
@@ -87,7 +102,7 @@ export const userData: UserData = {
       education: '本科',
       post: '计算机科学与技术',
       time: ['2021.09.01', '2023.06.30'],
-      content: '<p><br></p>',
+      content: '<p>独立开发雪花起始页和简历生成器，本简历由该项目生成。</p>',
       mode: '全日制',
     },
   ],
