e3c49891d13920f4abff6f4bcb63ff3440613cf3
y <1611037570@qq.com>
2026-05-03 07:10:42 +0800

    refactor(resume/editor): 将社交账号组件从数组结构重构为单条数据模型

--- src/views/resume/builder/page/editor/components/account.vue
+++ src/views/resume/builder/page/editor/components/account.vue
@@ -4,59 +4,45 @@ import { getCurrentInstance } from 'vue'
 const { proxy } = getCurrentInstance()
 
 // 社交账号列表数据
-const accounts = defineModel('modelValue', {
-  type: Array,
-  default: () => [],
+const name = defineModel('name', {
+  type: String,
+  default: '',
 })
-if (!accounts.value) {
-  accounts.value = []
-}
-
-// 点击按钮增加一组社交账号输入框（平台和网址）
-const addAccount = () => {
-  accounts.value.push({
-    name: '',
-    url: '',
-  })
-}
+const url = defineModel('url', {
+  type: String,
+  default: '',
+})
+const currentIndex = inject('df/current/index')
+const removeItem = inject('df/removeItem')
 
 // 删除社交账号
 const removeAccount = (index) => {
   proxy.$confirm('确定要删除当前内容吗？', '删除确认').then(() => {
-    accounts.value.splice(index, 1)
+    removeItem(index)
   })
 }
 </script>
 
 <template>
-  <div class="flex w-full flex-col">
-    <div class="flex flex-col gap-3">
-      <div v-for="(item, index) in accounts" :key="index" class="flex items-center gap-3">
-        <!-- 第一个是平台 -->
-        <div class="min-w-0 flex-1">
-          <SfInput v-model="item.name" placeholder="平台" />
-        </div>
-        <!-- 第二个是网址 -->
-        <div class="min-w-0 flex-1">
-          <SfInput v-model="item.url" placeholder="网址" />
-        </div>
-        <!-- 删除按钮 -->
-        <SfIcon
-          icon="ic:round-delete"
-          size="4"
-          boxSize="8"
-          class="shrink-0 cursor-pointer rounded-lg transition-colors hover:text-sf-theme"
-          @click="removeAccount(index)"
-        />
-      </div>
+  <div class="flex items-center gap-3">
+    <SfIcon icon="icon-park:drag" size="4" class="item-drag mr-1 cursor-move!" @click.stop="" />
+    <!-- 第一个是平台 -->
+    <div class="min-w-0 flex-1">
+      <SfInput v-model="name" placeholder="平台" />
     </div>
-    <div class="flex cursor-pointer items-center gap-1 text-sf-theme" @click="addAccount">
-      <SfIcon icon="ic:round-add" size="4" />
-      <span> 增加社交账号 </span>
+    <!-- 第二个是网址 -->
+    <div class="min-w-0 flex-1">
+      <SfInput v-model="url" placeholder="网址" />
     </div>
+    <!-- 删除按钮 -->
+    <SfIcon
+      icon="ic:round-delete"
+      size="4"
+      boxSize="8"
+      class="shrink-0 cursor-pointer rounded-lg transition-colors hover:text-sf-theme"
+      @click="removeAccount(currentIndex)"
+    />
   </div>
 </template>
 
-<style lang="scss" scoped>
-/* 样式遵循 Tailwind CSS，无需额外 CSS */
-</style>
+<style lang="scss" scoped></style>
