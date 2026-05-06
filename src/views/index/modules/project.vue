40c8b0ad064d6c4a00cced11e9326794cde03033
y <1611037570@qq.com>
2026-05-07 03:00:28 +0800

    refactor(project): 移除ProjectCard组件并简化项目展示逻辑

--- src/views/index/modules/project.vue
+++ src/views/index/modules/project.vue
@@ -10,11 +10,20 @@
         <div class="ml-6 h-[1px] flex-1 bg-gradient-to-r from-sf-border/50 to-transparent"></div>
       </div>
       <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
-        <ProjectCard
-          v-for="(item, index) in PROJECT_PAGE.filter((item) => item.url != '/index')"
+        <!-- <ProjectCard:data="item" /> -->
+        <div
+          v-for="(item, index) in list"
           :key="index"
-          :data="item"
-        />
+          class="cursor-pointer rounded-xl p-2 transition hover:bg-sf-bg-hover"
+        >
+          <div class="mb-1 flex items-center font-medium text-sf-base">
+            <SfLogo v-if="item.icon" :name="item.icon" :size="5" class="mr-2 shrink-0" />
+            <div class="truncate">
+              {{ item.name }}
+            </div>
+          </div>
+          <div class="text-sm text-sf-text-2" v-if="item.desc">{{ item.desc }}</div>
+        </div>
       </div>
     </div>
 
@@ -25,7 +34,17 @@
         <div class="ml-6 h-[1px] flex-1 bg-gradient-to-r from-sf-border/50 to-transparent"></div>
       </div>
       <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
-        <ProjectCard v-for="(item, index) in projectList" :key="index" :data="item" />
+        <div
+          v-for="(item, index) in projectList"
+          :key="index"
+          class="cursor-pointer rounded-xl p-2 transition hover:bg-sf-bg-hover"
+        >
+          <div class="mb-1 flex items-center font-medium text-sf-base">
+            <div class="truncate">
+              {{ item.name }}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -33,20 +52,26 @@
 
 <script setup>
 import { PROJECT_PAGE } from '@/constants'
-import ProjectCard from '../components/projectCard.vue'
 import SmallTitle from '../components/smallTitle.vue'
-
+const list = computed(() => {
+  return PROJECT_PAGE.value.filter((item) => {
+    if (item.hidden) {
+      return false
+    }
+    if (item.url != '/index') {
+      return item
+    }
+  })
+})
 // 项目经历数据
 const projectList = [
   {
-    name: '短说社区',
-    url: 'https://osx.opensns.cn/',
+    name: '**社区',
     desc: '一款集社区论坛、商城、学堂于一体的社交系统',
     urlType: 'web',
   },
   {
-    name: '天天工作台',
-    url: 'https://www.apps.vip/',
+    name: '**工作台',
     desc: '副屏"操作系统"',
   },
 ]
