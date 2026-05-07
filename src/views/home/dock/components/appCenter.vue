ad2d97317def9375876a900aa6e3bf81fe791d94
y <1611037570@qq.com>
2026-05-07 14:20:52 +0800

    refactor(组件): 重构应用中心相关组件

--- src/views/home/dock/components/appCenter.vue
+++ src/views/home/dock/components/appCenter.vue
@@ -1,19 +1,13 @@
 <template>
-  <div>
+  <SfMore>
     <div
       class="flex h-10 w-10 items-center justify-center rounded-lg bg-white/80 text-[10px] backdrop-blur-md transition-all duration-200"
-      @click="visible = true"
     >
-      添加
+      更多内容
     </div>
-    <AppCenter v-model="visible" />
-  </div>
+  </SfMore>
 </template>
 
-<script setup>
-import AppCenter from '@/views/home/appCenter/index.vue'
-
-const visible = ref(false)
-</script>
+<script setup></script>
 
 <style scoped></style>
--- src/views/home/dock/components/sendResume.vue
+++ src/views/home/dock/components/sendResume.vue
@@ -3,7 +3,7 @@
     class="flex h-10 w-10 items-center justify-center rounded-lg bg-white/80 text-[10px] backdrop-blur-md transition-all duration-200"
     @click="sendResume"
   >
-    投简历
+    B
   </div>
 </template>
 
