ba5995c19f13b18fb712f98ad545576509929080
y <1611037570@qq.com>
2026-07-20 23:19:11 +0800

    fix(loadElLocal): 修复动态切换语言时element-plus本地化未同步的问题

--- src/utils/modules/loadElLocal.ts
+++ src/utils/modules/loadElLocal.ts
@@ -2,7 +2,6 @@ import { DEFAULT_LANGUAGE } from '@/constants'
 import i18n from '@/locales'
 import en from 'element-plus/es/locale/lang/en'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import('element-plus/es/locale/lang/zh-cn')
 
 // 语言配置对象
 const langConfig: any = {
@@ -11,24 +10,20 @@ const langConfig: any = {
 }
 
 // 当前的语言
-const language = ref(DEFAULT_LANGUAGE)
-// 获取当前语言
+export const language = ref(DEFAULT_LANGUAGE)
+// 全局监听i18n语言切换
+watch(
+  () => i18n.global.locale.value,
+  (newLocale) => {
+    language.value = newLocale
+  },
+  { immediate: true },
+)
 export function getCurrentLocale() {
   const currentLocale: any = ref({})
-  watch(
-    () => i18n.global.locale.value,
-    async (newLocale) => {
-      currentLocale.value = newLocale
-    },
-    { immediate: true },
-  )
+
   return currentLocale
 }
-
-// 加载element-plus的locale
-onMounted(async () => {
-  language.value = await getCurrentLocale()
-})
 
 // 根据语言键获取配置，不存在则使用默认语言
 export const getElLocale = computed(() => langConfig[language.value || DEFAULT_LANGUAGE])
