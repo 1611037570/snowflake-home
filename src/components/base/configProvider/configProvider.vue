fcd4e923ae45a8302c42c0b52cb00abc3d8a7136
y <1611037570@qq.com>
2026-07-20 22:35:07 +0800

    refactor(configProvider): 调整语言计算逻辑

--- src/components/base/configProvider/configProvider.vue
+++ src/components/base/configProvider/configProvider.vue
@@ -1,29 +1,16 @@
 <script setup>
-import { DEFAULT_BACKGROUND, DEFAULT_COLOR, DEFAULT_LANGUAGE } from '@/constants'
-import { loadElLocale } from '@/utils'
-
-import en from 'element-plus/es/locale/lang/en'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { DEFAULT_BACKGROUND, DEFAULT_COLOR } from '@/constants'
+import { getElLocale } from '@/utils'
 
 provide('bg', DEFAULT_BACKGROUND)
 provide('color', DEFAULT_COLOR)
 
 // 加载element-plus的locale
-onMounted(async () => {
-  language.value = await loadElLocale()
-})
+const locale = getElLocale.value
+
 // https://cdn.jsdelivr.net/npm/pinyin@4.0.0/lib/umd/pinyin.min.js
 // https://www.bootcdn.cn/
 // https://www.jsdelivr.com/?query=vue
-// 语言配置对象
-const langConfig = {
-  'zh-CN': zhCn,
-  en: en,
-}
-
-const language = ref(DEFAULT_LANGUAGE)
-// 根据语言键获取配置，不存在则使用默认语言
-const locale = computed(() => langConfig[language.value] || langConfig[DEFAULT_LANGUAGE])
 </script>
 
 <template>
