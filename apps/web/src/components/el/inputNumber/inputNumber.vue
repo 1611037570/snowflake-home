683964d57abf75982e82fbc7c5af07a9ab867694
y <1611037570@qq.com>
2026-08-22 12:56:53 +0800

    feat(components): 新增封装的数字输入框组件SfInputNumber

---
+++ apps/web/src/components/el/inputNumber/inputNumber.vue
@@ -0,0 +1,45 @@
+<template>
+  <Component
+    class="rounded-xl text-sf-text"
+    :class="bg"
+    spellcheck="false"
+    :is="h(ElInputNumber, { ...$attrs, ref: changeRef }, $slots)"
+  />
+</template>
+
+<script setup lang="ts">
+import { ElInputNumber } from "element-plus";
+import type { ComponentInstance } from "vue";
+import { getCurrentInstance, h } from "vue";
+
+defineOptions({ name: "SfInputNumber" });
+
+const bg = inject("bg");
+const vm: any = getCurrentInstance();
+
+function changeRef(exports: any) {
+  vm.exposed = exports;
+}
+defineExpose({} as ComponentInstance<typeof ElInputNumber>);
+</script>
+
+<style scoped lang="scss">
+/* 输入框容器 */
+:deep(.el-input__wrapper) {
+  /* 移除阴影 */
+  box-shadow: none;
+  /* 移除背景 */
+  background-color: transparent;
+}
+
+/* 输入框文本 */
+:deep(.el-input__inner) {
+  /* 更改文本颜色 */
+  color: var(--color-sf-text);
+}
+/* 输入框占位符 */
+:deep(.el-input__inner::placeholder) {
+  /* 更改占位符颜色 */
+  /* color: var(--color-sf-text); */
+}
+</style>
