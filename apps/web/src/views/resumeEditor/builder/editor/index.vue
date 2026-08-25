764030210f2268f801f1619d102ddb891683c97f
y <1611037570@qq.com>
2026-08-25 13:28:36 +0800

    refactor(resumeEditor): 移除工作、项目、自定义模块组件

--- apps/web/src/views/resumeEditor/builder/editor/index.vue
+++ apps/web/src/views/resumeEditor/builder/editor/index.vue
@@ -4,21 +4,15 @@ import { storeToRefs } from "pinia";
 import Account from "./components/account.vue";
 import AddModule from "./components/addModule.vue";
 import BoxCollapse from "./components/boxCollapse.vue";
-import Custom from "./components/custom.vue";
 import Education from "./components/education.vue";
 import ItemCollapse from "./components/itemCollapse.vue";
-import Project from "./components/project.vue";
-import Work from "./components/work.vue";
 
 const resumeStore = useResumeStore();
 const { currentData, currentConfig, currentFixedConfig } = storeToRefs(resumeStore);
 
 // 注入到动态表单的自定义组件库
 const dynamicComponents = {
-  work: Work,
   education: Education,
-  project: Project,
-  custom: Custom,
   boxCollapse: BoxCollapse,
   itemCollapse: ItemCollapse,
   account: Account,
