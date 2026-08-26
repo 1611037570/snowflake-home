b20e5e426d742abb1c14ac1074b447f23b44fb3e
y <1611037570@qq.com>
2026-08-27 06:08:22 +0800

    feat(upload): 新增封装的Element Plus上传组件

---
+++ apps/web/src/components/el/upload/upload.vue
@@ -0,0 +1,21 @@
+<template>
+  <Component :is="h(ElUpload, { ...$attrs, ref: changeRef }, $slots)" />
+</template>
+
+<script setup lang="ts">
+import { ElUpload } from "element-plus";
+import type { ComponentInstance } from "vue";
+import { getCurrentInstance, h } from "vue";
+
+defineOptions({ name: "SfUpload" });
+
+const vm: any = getCurrentInstance();
+
+function changeRef(exports: any) {
+  vm.exposed = exports;
+}
+
+defineExpose({} as ComponentInstance<typeof ElUpload>);
+</script>
+
+<style scoped></style>
