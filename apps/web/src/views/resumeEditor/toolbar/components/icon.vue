9d9ee8e7dce74934c6a3640993c7c8d70607b060
y <1611037570@qq.com>
2026-08-27 00:45:10 +0800

    refactor(icon组件): 优化图标组件的默认配置与props支持

--- apps/web/src/views/resumeEditor/toolbar/components/icon.vue
+++ apps/web/src/views/resumeEditor/toolbar/components/icon.vue
@@ -2,25 +2,29 @@
 defineProps({
   icon: {
     type: String,
-    default: "iconamoon:settings-fill",
+    default: "",
   },
   content: {
     type: String,
-    default: "系统设置",
+    default: "",
+  },
+  size: {
+    type: Number,
+    default: 6,
   },
 });
 
-defineEmits(["click"]);
+defineEmits(["onClick"]);
 </script>
 
 <template>
   <SfTooltip :content="content" placement="left">
     <SfIcon
       :icon="icon"
-      size="6"
+      :size="size"
       boxSize="8"
       class="rounded-full bg-sf-bg-2 text-sf-text-2 transition-all duration-400 hover:scale-115 hover:bg-sf-theme-2 hover:text-sf-theme-text"
-      @click="emit('click')"
+      @click="emit('onClick')"
     />
   </SfTooltip>
 </template>
