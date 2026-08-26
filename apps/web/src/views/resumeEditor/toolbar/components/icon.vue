a20aa9d10b1dccda23e30dd52e30bce920b123c9
y <1611037570@qq.com>
2026-08-27 00:32:01 +0800

    feat(resumeEditor/toolbar): 新增通用图标工具栏组件

---
+++ apps/web/src/views/resumeEditor/toolbar/components/icon.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    default: "iconamoon:settings-fill",
+  },
+  content: {
+    type: String,
+    default: "系统设置",
+  },
+});
+
+defineEmits(["click"]);
+</script>
+
+<template>
+  <SfTooltip :content="content" placement="left">
+    <SfIcon
+      :icon="icon"
+      size="6"
+      boxSize="8"
+      class="rounded-full bg-sf-bg-2 text-sf-text-2 transition-all duration-400 hover:scale-115 hover:bg-sf-theme-2 hover:text-sf-theme-text"
+      @click="emit('click')"
+    />
+  </SfTooltip>
+</template>
+
+<style lang="scss" scoped></style>
