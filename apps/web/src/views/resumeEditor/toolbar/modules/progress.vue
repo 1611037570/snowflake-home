3b8cd20b92eace7172276fe5ad24e137ad28d195
y <1611037570@qq.com>
2026-08-27 03:25:59 +0800

    fix(progress toolbar): 调整进度条模块样式与显示数值

--- apps/web/src/views/resumeEditor/toolbar/modules/progress.vue
+++ apps/web/src/views/resumeEditor/toolbar/modules/progress.vue
@@ -7,13 +7,16 @@ const { system } = storeToRefs(resumeStore);
 </script>
 
 <template>
-  <div v-if="system.showProgress" class="absolute -top-12 -right-10 z-50 -translate-y-1/2 transform">
+  <div
+    v-if="system.showProgress"
+    class="absolute -top-12 -right-10 z-50 -translate-y-1/2 transform"
+  >
     <div
-      class="h-[70px] w-[90px] cursor-pointer rounded-2xl bg-linear-to-r from-blue-500 to-purple-500 p-3.5 text-white transition-all duration-300 hover:scale-105"
+      class="h-[66px] w-[90px] cursor-pointer rounded-2xl bg-linear-to-r from-blue-500 to-purple-500 p-3 text-white transition-all duration-300 hover:scale-105"
     >
       <div class="flex w-10 flex-col items-center justify-center">
         <div class="flex items-center text-[16px] font-bold">
-          <span>99</span><span class="text-[14px]">%</span>
+          <span>100</span><span class="text-[14px]">%</span>
         </div>
         <div class="text-[11px] opacity-90">完成度</div>
       </div>
