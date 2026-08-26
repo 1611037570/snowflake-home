4a8d358f444a647fe897fc7f77bc3e72fe6897ea
y <1611037570@qq.com>
2026-08-27 01:56:22 +0800

    feat(resumeEditor/toolbar): 新增导出和导入简历的弹窗容器

--- apps/web/src/views/resumeEditor/toolbar/modules/exportResume.vue
+++ apps/web/src/views/resumeEditor/toolbar/modules/exportResume.vue
@@ -1,9 +1,16 @@
 <script setup>
 import Icon from "../components/icon.vue";
+import { ref } from "vue";
+
+const visible = ref(false);
 </script>
 
 <template>
-  <Icon icon="fa6-solid:file-export" size="4" content="导出简历" />
+  <Icon icon="fa6-solid:file-export" size="4" content="导出简历" @click="visible = true" />
+
+  <SfModal v-model="visible" title="导出简历">
+    <div class="flex w-[400px] flex-col gap-5 p-4"></div>
+  </SfModal>
 </template>
 
 <style lang="scss" scoped></style>
--- apps/web/src/views/resumeEditor/toolbar/modules/importResume.vue
+++ apps/web/src/views/resumeEditor/toolbar/modules/importResume.vue
@@ -1,9 +1,16 @@
 <script setup>
 import Icon from "../components/icon.vue";
+import { ref } from "vue";
+
+const visible = ref(false);
 </script>
 
 <template>
-  <Icon icon="fa6-solid:file-import" size="4" content="导入简历" />
+  <Icon icon="fa6-solid:file-import" size="4" content="导入简历" @click="visible = true" />
+
+  <SfModal v-model="visible" title="导入简历">
+    <div class="flex w-[400px] flex-col gap-5 p-4"></div>
+  </SfModal>
 </template>
 
 <style lang="scss" scoped></style>
