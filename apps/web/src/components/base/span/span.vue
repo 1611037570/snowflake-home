5bd9fb64072c7384f848d77a3b6bba18b0002a92
y <1611037570@qq.com>
2026-09-04 13:41:06 +0800

    style(span组件): 新增下划线高度可配置属性并优化样式
    1. 新增underlineHeight props自定义下划线高度
    2. 重构props定义方式，添加计算属性处理尺寸转换
    3. 调整容器和下划线的样式类与布局
    4. 优化hover过渡动画的配置

--- apps/web/src/components/base/span/span.vue
+++ apps/web/src/components/base/span/span.vue
@@ -1,20 +1,23 @@
 <script setup>
 defineOptions({ name: "SfSpan" });
-defineProps({
+const props = defineProps({
   active: { type: Boolean, default: false },
+  // 下划线高度
+  underlineHeight: { type: Number, default: 0.5 },
 });
+const height = computed(() => `${props.underlineHeight * 4}px`);
 </script>
-
 <template>
   <span
     :class="[active ? 'text-sf-theme' : '']"
-    class="group relative cursor-pointer font-medium transition-all hover:text-sf-theme"
+    class="group flex-c relative cursor-pointer transition-all duration-200 hover:text-sf-theme"
   >
     <slot />
     <!-- 悬停下划线动画 -->
     <span
-      class="absolute bottom-0 left-1/2 h-0.5 w-0 -translate-x-1/2 rounded-full bg-sf-theme transition-all duration-200 ease-out group-hover:w-full"
+      class="absolute -bottom-1 left-1/2 w-0 -translate-x-1/2 rounded-full bg-sf-theme ease-out group-hover:w-full"
       :class="[active ? 'w-full' : '']"
+      :style="{ height: height }"
     ></span>
   </span>
 </template>
