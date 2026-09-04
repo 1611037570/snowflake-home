987668a91b5e69f7551f541d01ec6df45ff185ce
y <1611037570@qq.com>
2026-09-04 12:59:04 +0800

    feat(base-button): 新增按钮组件的样式配置与图标支持
    新增plain、round、icon属性，重构按钮样式绑定逻辑，支持主题/错误类型的普通与描边样式切换

--- apps/web/src/components/base/button/button.vue
+++ apps/web/src/components/base/button/button.vue
@@ -2,21 +2,46 @@
 const props = defineProps({
   type: {
     type: String,
-    default: "primary",
+    default: "theme",
+  },
+  plain: {
+    type: Boolean,
+    default: false,
+  },
+  round: {
+    type: Boolean,
+    default: true,
   },
   border: {
     type: Boolean,
     default: false,
   },
+  icon: {
+    type: String,
+    default: "",
+  },
 });
 const isBorder = computed(() => (props.border ? "border border-sf-b" : ""));
+const isRound = computed(() => (props.round ? "rounded-3xl" : "rounded-xl"));
+const plainClassObj = {
+  error: "bg-sf-error-2 text-sf-error hover:bg-sf-error hover:text-white",
+  theme: "bg-sf-theme-3 text-sf-theme hover:bg-sf-theme hover:text-white",
+};
+const classObj = {
+  error: "",
+  theme: "bg-sf-theme text-sf-theme-text hover:bg-sf-theme-2",
+};
+const getClass = computed(() => {
+  return props.plain ? plainClassObj[props.type] : classObj[props.type];
+});
 </script>
 
 <template>
   <div
-    class="flex cursor-pointer items-center justify-center rounded-3xl bg-sf-theme px-3 py-2 text-sm text-sf-theme-text transition-all duration-300 hover:bg-sf-theme-2 active:scale-98"
-    :class="isBorder"
+    class="flex cursor-pointer items-center justify-center px-3 py-2 text-sm transition-all duration-300 active:scale-98"
+    :class="[isBorder, getClass, isRound]"
   >
+    <SfIcon v-if="icon" :icon="icon" size="4" class="mr-1" />
     <slot></slot>
   </div>
 </template>
