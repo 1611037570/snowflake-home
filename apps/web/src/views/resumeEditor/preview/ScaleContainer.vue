587e38a8cec12f85d99e9e3f985ad32ebc666985
y <1611037570@qq.com>
2026-09-08 18:39:40 +0800

    fix(ScaleContainer): 添加工具栏显示控制逻辑
    给工具栏容器添加v-show指令控制其显示隐藏

--- apps/web/src/views/resumeEditor/preview/ScaleContainer.vue
+++ apps/web/src/views/resumeEditor/preview/ScaleContainer.vue
@@ -129,6 +129,7 @@ useResizeObserver(contentRef, ([entry]) => {
   <div class="group relative mx-1 flex h-full flex-col self-stretch overflow-hidden">
     <div
       class="mx-auto mb-3 flex items-center gap-1 self-stretch rounded-full border border-sf-b bg-sf-primary p-1.5 select-none"
+      v-show="showToolbar"
     >
       <SfDropdown trigger="hover" placement="bottom-start" :show-arrow="false">
         <SfTooltip :content="`切换简历语言`">
