066e186b6dacab2ad1f421341767eb1484d22a3b
y <1611037570@qq.com>
2026-09-15 18:09:00 +0800

    fix(resume): 修正视频作品字段文案

--- apps/web/src/views/resume/editor/builder/editor/components/video.vue
+++ apps/web/src/views/resume/editor/builder/editor/components/video.vue
@@ -19,18 +19,18 @@ const desc = defineModel("desc", {
   <div class="flex w-full flex-col gap-1">
     <div class="flex items-center gap-3">
       <div class="w-[100px] min-w-0">
-        <SfFormItem label="链接" prop="url">
-          <SfInput v-model="name" placeholder="视频名称" />
+        <SfFormItem label="视频名称" prop="url">
+          <SfInput v-model="name" placeholder="请输入视频名称" />
         </SfFormItem>
       </div>
       <div class="min-w-0 flex-1">
-        <SfFormItem label="线上链接" prop="url">
-          <SfInput v-model="url" placeholder="视频链接" />
+        <SfFormItem label="视频链接" prop="url">
+          <SfInput v-model="url" placeholder="请输入视频链接" />
         </SfFormItem>
       </div>
     </div>
-    <SfFormItem label="介绍" prop="desc">
-      <SfInput v-model="desc" placeholder="视频介绍" />
+    <SfFormItem label="视频介绍" prop="desc">
+      <SfInput v-model="desc" placeholder="请输入视频介绍" />
     </SfFormItem>
   </div>
 </template>
