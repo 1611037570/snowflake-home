9a858035ca4ed643ed3010d32ada7d9baf745b70
y <1611037570@qq.com>
2026-09-17 01:53:23 +0800

    feat(userAgreement): 新增用户协议弹窗组件

---
+++ apps/web/src/components/business/userAgreement/userAgreement.vue
@@ -0,0 +1,12 @@
+<script setup>
+const visible = ref(false);
+</script>
+
+<template>
+  <div>
+    <div @click="visible = true">用户协议</div>
+    <SfModal v-model="visible"></SfModal>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
