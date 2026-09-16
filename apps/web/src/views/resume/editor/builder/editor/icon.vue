1eed95e7a6a1931b57c58ba4b0fc5f1eead49518
y <1611037570@qq.com>
2026-09-17 02:03:38 +0800

    feat(resume-editor): 新增图标组件封装

---
+++ apps/web/src/views/resume/editor/builder/editor/icon.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["click"]);
+
+function handleClick() {
+  emit("click");
+}
+</script>
+
+<template>
+  <SfIcon
+    :icon="icon"
+    size="4"
+    boxSize="6"
+    @pointerdown.stop.prevent
+    @click.stop.prevent="handleClick"
+    class="cursor-pointer rounded-xl hover:bg-sf-theme hover:text-sf-theme-text"
+  ></SfIcon>
+</template>
+
+<style lang="scss" scoped></style>
