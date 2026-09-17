5256472374c189680f78c39f54db8be9f24d49a3
y <1611037570@qq.com>
2026-09-17 11:04:57 +0800

    feat(resume/preview): 新建简历预览页的统一标题

---
+++ apps/web/src/views/resume/editor/preview/components/itemTitle.vue
@@ -0,0 +1,18 @@
+<script setup>
+// 统一标题组件
+import ResumeField from "./resumeField/index.vue";
+defineProps({
+  name: {
+    default: "",
+  },
+});
+const fontValue = inject("fontValue");
+</script>
+
+<template>
+  <div class="font-bold" :style="[fontValue(1)]">
+    <ResumeField :model-value="name" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
